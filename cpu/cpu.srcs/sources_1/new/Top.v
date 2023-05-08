12d3b1f45e6c66eb675bda88c2a31ed59e7a5819
cswccc <3344767250@qq.com>
2023-05-09 00:31:12 +0800

    Set up project in vivado.

---
+++ cpu/cpu.srcs/sources_1/new/Top.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/08 21:49:49
+// Design Name: 
+// Module Name: Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top(
+
+    );
+    
+endmodule
