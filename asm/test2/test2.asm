16e39b09630c45c50a508b677df518ed8b2a72d0
cswccc <3344767250@qq.com>
2023-05-18 23:27:14 +0800

    update

--- asm/test2/test2.asm
+++ asm/test2/test2.asm
@@ -10,26 +10,50 @@
 	addi $t6, $zero, 6
 	addi $t7, $zero, 7
 	
+second:	addi $t9, $zero, 3
+	sll $t9, $t9, 23
+	
+loop_s:	addi $t9, $t9, -1
+	bne $t9, $zero, loop_s
+	
+	jr $ra
+
+# s0: read a
+# s1: push stack and pop stack times
+# s2: the add sum
+# s3: now number
+# s7: case type
 recru:	addi $sp, $sp, -8
 	sw $ra, 4($sp)
 	sw $s3, 0($sp)
-	addi $s1, $s1, 2
+	addi $s1, $s1, 2 # push stack for two times
 	
 	bne $s7, $t2, not2 # push stack for case 2
-	addi $v0, $zero, 3
-	add $a0, $zero, $s3
-	addi $zero, $zero, 12345
-	# 2~3 seconds
+		addi $v0, $zero, 3
+		add $a0, $zero, $s3
+		addi $zero, $zero, 12345
+		# 2~3 seconds
+		addi $t8, $zero, 2
+		sec2_1:	addi $t8, $t8, -1
+			jal second
+			bne $t8, $zero, sec2_1
+		lw $ra, 4($sp)
 	
-not2:	bne $s3, $0, contin
+not2:	bne $s3, $s0, contin
 	add $s2, $s2, $s3
 	addi $sp, $sp, 8
+	addi $s1, $s1, 2 # pop stack for two times
 	
 	bne $s7, $t3, not3_1 # pop stack for case 3
-	addi $v0, $zero, 3
-	add $a0, $zero, $s3
-	addi $zero, $zero, 12345
-	# 2~3 seconds
+		addi $v0, $zero, 3
+		add $a0, $zero, $s3
+		addi $zero, $zero, 12345
+		# 2~3 seconds
+		addi $t8, $zero, 2
+		sec2_2:	addi $t8, $t8, -1
+			jal second
+			bne $t8, $zero, sec2_2
+		lw $ra, 4($sp)
 not3_1:	jr $ra
 	
 contin:	addi $s3, $s3, 1
@@ -39,13 +63,18 @@ contin:	addi $s3, $s3, 1
 	lw $ra, 4($sp)
 	addi $sp, $sp, 8
 	add $s2, $s2, $s3
-	addi $s1, $s1, 2
+	addi $s1, $s1, 2 # pop stack for two times
 	
 	bne $s7, $t3, not3_2 # pop stack for case 3
-	addi $v0, $zero, 3
-	add $a0, $zero, $s3
-	addi $zero, $zero, 12345
-	# 2~3 seconds
+		addi $v0, $zero, 3
+		add $a0, $zero, $s3
+		addi $zero, $zero, 12345
+		# 2~3 seconds
+		addi $t8, $zero, 2
+		sec2_3:	addi $t8, $t8, -1
+			jal second
+			bne $t8, $zero, sec2_3
+		lw $ra, 4($sp)
 not3_2:	jr $ra
 
 While:  addi $v0, $zero, 0
@@ -60,6 +89,9 @@ While:  addi $v0, $zero, 0
 	beq $t7, $a0, case7
 	j While
 
+# s0: store the value of a
+# s2: store the sum from 1 to a
+# s3: used during adding loop
 case0:	addi $v0, $zero, 0
 	addi $zero, $zero, 12345
 	
@@ -67,12 +99,12 @@ case0:	addi $v0, $zero, 0
 	add $s2, $zero, $zero
 	addi $s3, $zero, 1
 	
-	slt $s1, $s0, $zero
+	slt $s1, $s0, $zero # check whether a < 0
 	beq $s1, $zero, big0
-	addi $v0, $zero, 4
+	addi $v0, $zero, 4 # show a < 0
 	addi $a0, $zero, 2
 	addi $zero, $zero, 12345
-	loop00:	add $s2, $s2, $s3
+	loop00:	add $s2, $s2, $s3 
 		beq $s3, $s0, break0
 		addi $s3, $s3, -1
 		j loop00
@@ -85,6 +117,11 @@ break0:	add $v0, $zero, 2
 	addi $zero, $zero, 12345
 	j While
 
+# s0: read a
+# s1: push stack and pop stack times
+# s2: the add sum
+# s3: now number
+# s7: case type
 case1:	addi $v0, $zero, 1
 	addi $zero, $zero, 12345
 	
@@ -92,6 +129,7 @@ case1:	addi $v0, $zero, 1
 	add $s1, $zero, $zero
 	add $s2, $zero, $zero
 	addi $s3, $zero, 1
+	addi $s7, $zero, 1
 	
 	jal recru
 	
@@ -100,11 +138,15 @@ case1:	addi $v0, $zero, 1
 	addi $zero, $zero, 12345
 	
 	j While
-	
+
+# s0: read a
+# s2: the add sum
+# s3: now number
+# s7: case type
 case2:	addi $v0, $zero, 1
 	addi $zero, $zero, 12345
 	
-	addi $7, $zero, 2
+	addi $s7, $zero, 2
 	
 	add $s0, $a0, $zero
 	add $s2, $zero, $zero
@@ -113,11 +155,15 @@ case2:	addi $v0, $zero, 1
 	jal recru
 	
 	j While
-	
+
+# s0: read a
+# s2: the add sum
+# s3: now number
+# s7: case type
 case3:	addi $v0, $zero, 1
 	addi $zero, $zero, 12345
 	
-	addi $7, $zero, 3
+	addi $s7, $zero, 3
 	
 	add $s0, $a0, $zero
 	add $s2, $zero, $zero
@@ -126,83 +172,162 @@ case3:	addi $v0, $zero, 1
 	jal recru
 	
 	j While
-	
+
+# s0: a
+# s1: b
+# s2: a + b
+# s3: higher 24 bits of (a + b)
 case4:	addi $v0, $zero, 0 # get a
 	addi $zero, $zero, 12345
 	add $s0, $a0, $zero
 	
-	addi $v0, $zero, 2 # show a
-	addi $zero, $zero, 12345
-	
-	addi $v0, $zero, 4 # show whether a is negative
-	srl $a0, $a0, 31
-	addi $zero, $zero, 12345
-	
 	addi $v0, $zero, 0 # get b
 	addi $zero, $zero, 12345
 	add $s1, $a0, $zero
 	
-	addi $v0, $zero, 2 # show b
-	addi $zero, $zero, 12345
-	
-	addi $v0, $zero, 4 # show whether b is negative
-	srl $a0, $a0, 31
-	addi $zero, $zero, 12345
-	
 	add $s2, $s1, $s0
+	
+	srl $s3, $s2, 31
+	srl $s4, $s1, 31
+	srl $s5, $s0, 31
+	
+	beq $s3, $zero, noov4 # check whether overflow
+		addi $v0, $zero, 4 # overflow
+		addi $a0, $zero, 1
+		addi $zero, $zero, 12345
+	
+noov4:	sll $s2, $s2, 24
+	sra $s2, $s2, 24
 	
 	addi $v0, $zero, 2 # show a + b
 	add $a0, $s2, $zero
-	addi $zero, $zero, 12345
-	
-	srl $s2, $s2, 31
-	srl $s1, $s1, 31
-	srl $s0, $s0, 31
-	
-	nor $s3, $s1, $s0
-	and $s3, $s3, $s2 # pos + pos = neg
-	
-	and $s4, $s1, $s0
-	nor $s5, $s2, $s2
-	and $s4, $s4, $s5 # neg + neg = pos
-	
-	or $a0, $s3, $s4
-	
-	addi $v0, $zero, 4
 	addi $zero, $zero, 12345
 	
 	j While
 
+# s0: a
+# s1: b
+# s2: a - b
+# s3: higher 24 bits of (a - b)
 case5:	addi $v0, $zero, 0 # get a
 	addi $zero, $zero, 12345
 	add $s0, $a0, $zero
-	
-	addi $v0, $zero, 2 # show a
-	addi $zero, $zero, 12345
-	
-	addi $v0, $zero, 4 # show whether a is negative
-	srl $a0, $a0, 31
-	addi $zero, $zero, 12345
 	
 	addi $v0, $zero, 0 # get b
 	addi $zero, $zero, 12345
 	add $s1, $a0, $zero
 	
-	addi $v0, $zero, 2 # show b
-	addi $zero, $zero, 12345
-	
-	addi $v0, $zero, 4 # show whether b is negative
-	srl $a0, $a0, 31
-	addi $zero, $zero, 12345
-	
 	sub $s2, $s1, $s0
 	
-	addi $v0, $zero, 2 # show a + b
+	srl $s3, $s3
+	
+	beq $s3, $zero, noov5 # check whether overflow
+		addi $v0, $zero, 4 # overflow
+		addi $a0, $zero, 1
+		addi $zero, $zero, 12345
+	
+noov5:	sll $s2, $s2, 24
+	sra $s2, $s2, 24
+	
+	addi $v0, $zero, 2 # show a - b
 	add $a0, $s2, $zero
 	addi $zero, $zero, 12345
 	
-	srl $s2, $s2, 31
-	srl $s1, $s1, 31
-	srl $s0, $s0, 31
+	j While
+
+# a * b:
+# $s0: a
+# $s1: b
+# $s2: used to check whether a < 0 or b < 0, after that it is used to stand 1
+# $s3: used to record how many negative numbers
+# $s4: used to record the answer
+case6:	addi $v0, $zero, 0
+	addi $zero, $zero, 12345
+	add $s0, $zero, $a0
 	
+	addi $v0, $zero, 0
+	addi $zero, $zero 12345
+	add $s1, $zero, $a0
 	
+	slt $s2, $s0, $zero # check whether a is negative
+	beq $s2, $zero, no6_0
+	addi $s3, $zero, 1
+	sub $s0, $zero, $s0
+	
+no6_0:	slt $s2, $s1, $zero  # check whether a is negative
+	beq $s2, $zero, no6_1
+	addi $s3, $s3, 1
+	sub $s1, $zero, $s1
+
+no6_1:	add $s4, $zero, $zero
+	addi $s2, $zero, 1
+loop6:	beq $s0, $zero, break6
+	and $s5, $s0, $s2
+	beq $s5, $zero, else6_0
+	add $s4, $s4, $s1
+else6_0:srl $s0, $s0, 1
+	sll $s1, $s1, 1
+	j loop6
+break6:	addi $s5, $zero, 1
+	bne $s3, $s5, noneg6
+	sub $s4, $zero, $s4
+	
+noneg6:	addi $v0, $zero, 2 # show a * b
+	add $a0, $zero, $s4
+	addi $zero, $zero, 12345
+	
+	j While
+
+case7:	addi $v0, $zero, 0
+	addi $zero, $zero, 12345
+	add $s0, $zero, $a0
+	
+	addi $v0, $zero, 0
+	addi $zero, $zero 12345
+	add $s1, $zero, $a0
+	
+	slt $s2, $s0, $zero # check whether a is negative
+	beq $s2, $zero, no7_0
+	addi $s3, $zero, 1
+	sub $s0, $zero, $s0
+	
+no7_0:	slt $s2, $s1, $zero  # check whether a is negative
+	beq $s2, $zero, no7_1
+	addi $s3, $s3, 1
+	sub $s1, $zero, $s1
+	
+no7_1:	sll $s1, $s1, 8
+	add $s4, $zero, $zero
+	addi $s5, $zero, 8
+loop7:	addi $s5, $s5, -1
+	srl $s1, $s1, 1
+	sub $s0, $s0, $s1
+	slt $s7, $s0, $zero
+	beq $s7, $zero, noadd7
+	addi $s4, $s4, 1
+	add $s0, $s0, $s1
+noadd7:	sll $s4, $s4, 1
+	bne $s5, $zero, loop7
+
+	addi $s5, $zero, 1
+	bne $s3, $s5, notneg
+	sub $s4, $zero, $s4
+	
+notneg:	addi $v0, $zero, 2
+	add $a0, $zero, $s4
+	addi $zero, $zero, 12345
+	#5 seconds
+	addi $s5, $zero, 5
+sec5_1:	addi $s5, $s5, -1
+	jal second
+	bne $s5, $zero, sec5_1
+	
+	add $a0, $zero, $s0
+	addi $zero, $zero, 12345
+	#5 seconds
+	addi $s5, $zero, 5
+sec5_2:	addi $s5, $s5, -1
+	jal second
+	bne $s5, $zero, sec5_2
+	
+	j While
